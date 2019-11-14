036984428403469a683a1324178ebe39f347dbe4
Sam Hughes <sam@samuelhughes.com>
2019-11-14 12:13:15 -0800

    setup.rb options for commit and distro

--- setup.rb
+++ setup.rb
@@ -1,16 +1,35 @@
 #!/usr/bin/env ruby
 
-basedir = Dir.pwd
+require 'optparse'
 
-# TODO: Ensure we handle errors when `system calls` fail.
-
-Dir.chdir("rdbcheckout") {
-  system "docker build -t rdbcheckout ." or raise "build rdbcheckout fail"
+options = {
+  :commit => "v2.3.7",
+  :packages => false,
+  :distro => nil,
 }
+parser = OptionParser.new { |opts|
+  opts.banner = "Usage: ./setup.rb [options]"
+  opts.on("-c", "--commit COMMIT", "The commit to build packages for (default v2.3.7)") { |c|
+    options[:commit] = c
+  }
+  opts.on("--[no-]packages", "Build packages (default off)") { |p|
+    options[:packages] = p
+  }
+  opts.on("--distro DISTRO", "The distro to build packages for (default all)") { |d|
+    puts("distro option ", d)
+    if d == "all"
+      options[:distro] = nil
+    else
+      options[:distro] = d
+    end
+  }
+}
+
+parser.parse!
 
 # Building and packaging doesn't exactly belong here...
 
-commit = "v2.3.7"
+commit = options[:commit]
 build_args = "--build-arg commit=#{commit}"
 
 # distros is in order of priority.
@@ -30,17 +49,31 @@ distros = [
   "disco",
 ]
 
+if options[:distro] != nil
+  distros = distros.filter { |d| options[:distro] == d }
+end
 
-# First do builds
+if distros.empty?
+  raise "Invalid distro"
+end
+
+# First build the base image
+Dir.chdir("rdbcheckout") {
+  system "docker build -t rdbcheckout ." or raise "build rdbcheckout fail"
+}
+
+# Then do builds
 distros.each { |distro|
   Dir.chdir("#{distro}/build") {
     system "docker build -t rdb-#{distro}-build:#{commit} #{build_args} ." or raise "build rdb-#{distro}-build fail"
   }
 }
 
-# Then build packages
-distros.each { |distro|
-  Dir.chdir("#{distro}/package") {
-    system "docker build -t rdb-#{distro}-package:#{commit} #{build_args} ." or raise "build rdb-#{distro}-package fail"
+if options[:packages]
+  # Then build packages
+  distros.each { |distro|
+    Dir.chdir("#{distro}/package") {
+      system "docker build -t rdb-#{distro}-package:#{commit} #{build_args} ." or raise "build rdb-#{distro}-package fail"
+    }
   }
-}
+end
