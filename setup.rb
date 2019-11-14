3fc3e34c6b90a6f9b80da92b176f1bfebe13cee9
Sam Hughes <sam@samuelhughes.com>
2019-11-13 20:23:22 -0800

    Make Dockerfiles parameterized by commit

--- setup.rb
+++ setup.rb
@@ -10,12 +10,15 @@ Dir.chdir("rdbcheckout") {
 
 # Building and packaging doesn't exactly belong here...
 
+commit = "v2.3.7"
+build_args = "--build-arg commit=#{commit}"
+
 Dir.chdir("bionic/build") {
-  system "docker build -t rdb-bionic-build ." or raise "build rdb-bionic-build fail"
+  system "docker build -t rdb-bionic-build:#{commit} #{build_args} ." or raise "build rdb-bionic-build fail"
 }
 
 Dir.chdir("bionic/package") {
-  system "docker build -t rdb-bionic-package ." or raise "build rdb-bionic-package fail"
+  system "docker build -t rdb-bionic-package:#{commit} #{build_args} ." or raise "build rdb-bionic-package fail"
 }
 
 
