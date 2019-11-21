b1eb0945736487f9064e1aed8e492ecc0f9b428c
Sam Hughes <sam@samuelhughes.com>
2019-11-21 00:23:16 -0800

    Buildx multi-arch WIP

--- setup.rb
+++ setup.rb
@@ -3,6 +3,7 @@
 require 'optparse'
 
 # Pro tip: You might have to run "sudo setup.rb ..."
+# Pro tip: Use Docker Edge and follow the buildx instructions in README.
 
 # To save time and space, we build images for support libs for
 # specific commits like v2.3.7 and b2365be (for v2.4.x), instead of
@@ -15,7 +16,12 @@ options = {
   :builds => true,
   :packages => false,
   :distro => nil,
+  :arch => "amd64",
 }
+
+# Some v24 commit, used when fetching, and as the --v24support commit.
+v24_base_commit = "b2365bef6"
+
 parser = OptionParser.new { |opts|
   opts.banner = "Usage: ./setup.rb [options]"
   opts.on("-c", "--commit COMMIT", "The commit to build packages for (default v2.3.7)") { |c|
@@ -41,9 +47,16 @@ parser = OptionParser.new { |opts|
       options[:distro] = d
     end
   }
+  opts.on("--arch ARCH", "The arch (amd64, arm64) to build for (default amd64)") { |p|
+    if p == "amd64" || p == "arm64"
+      options[:arch] = p
+    else
+      raise "Invalid arch"
+    end
+  }
   opts.on("--v24support", "Build support libs for v2.4.x") { |b|
     # b2365be is the "Parallelize deb-build" commit in v2.4.x.
-    options[:support_commit] = "b2365bef6"
+    options[:support_commit] = v24_base_commit
   }
 }
 
@@ -51,6 +64,8 @@ parser.parse!
 
 commit = options[:commit]
 support_commit = options[:support_commit]
+arch = options[:arch]
+arch_args = "--platform linux/#{arch}"
 package_args = "--build-arg commit=#{commit}"
 build_args = "#{package_args} --build-arg support_commit=#{support_commit}"
 build_args_support = "--build-arg commit=#{support_commit}"
@@ -80,15 +95,17 @@ if distros.empty?
   raise "Invalid distro"
 end
 
+buildcmd = "docker buildx build #{arch_args}"
+
 # First build the base image
 Dir.chdir("rdbcheckout") {
-  system "docker build -t samrhughes/rdbcheckout ." or raise "build rdbcheckout fail"
+  system "#{buildcmd} -t samrhughes/rdbcheckout ." or raise "build rdbcheckout fail"
 }
 
 # Then do system builds
 distros.each { |distro|
   Dir.chdir("#{distro}/system") {
-    system "docker build -t samrhughes/rdb-#{distro}-system ." or raise "build rdb-#{distro}-system fail"
+    system "#{buildcmd} -t samrhughes/rdb-#{distro}-system ." or raise "build rdb-#{distro}-system fail"
   }
 }
 
@@ -96,7 +113,7 @@ if options[:support]
   # Then do support builds
   distros.each { |distro|
     Dir.chdir("#{distro}/support") {
-      system "docker build -t samrhughes/rdb-#{distro}-support:#{support_commit} #{build_args_support} ." or raise "build rdb-#{distro}-support fail"
+      system "#{buildcmd} -t samrhughes/rdb-#{distro}-support:#{support_commit} #{build_args_support} ." or raise "build rdb-#{distro}-support fail"
     }
   }
 
@@ -104,7 +121,7 @@ if options[:support]
     # Then do builds
     distros.each { |distro|
       Dir.chdir("#{distro}/build") {
-        system "docker build -t samrhughes/rdb-#{distro}-build:#{commit} #{build_args} ." or raise "build rdb-#{distro}-build fail"
+        system "#{buildcmd} -t samrhughes/rdb-#{distro}-build:#{commit} #{build_args} ." or raise "build rdb-#{distro}-build fail"
       }
     }
 
@@ -112,7 +129,7 @@ if options[:support]
       # Then build packages
       distros.each { |distro|
         Dir.chdir("#{distro}/package") {
-          system "docker build -t samrhughes/rdb-#{distro}-package:#{commit} #{package_args} ." or raise "build rdb-#{distro}-package fail"
+          system "#{buildcmd} -t samrhughes/rdb-#{distro}-package:#{commit} #{package_args} ." or raise "build rdb-#{distro}-package fail"
         }
       }
     end
